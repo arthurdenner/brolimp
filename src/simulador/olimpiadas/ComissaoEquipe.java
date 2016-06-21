5182713f30be39a6ad86697d756a2a9639cc4c2f
Emerson Laurentino <emersonlaurentino@hotmail.com>
2016-06-21 20:48:30 -0300

    criacao tabela de comissao

---
+++ src/simulador/olimpiadas/ComissaoEquipe.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simulador.olimpiadas;
+
+/**
+ *
+ * @author Aluno
+ */
+public class ComissaoEquipe {
+    
+    void getEquipesCadastradas() {
+        
+    }
+    
+    void cadastrarEquipe(String nomeEquipe) {
+        
+    }
+    
+    void removerEquipe(String nomeEquipe) {
+        
+    }
+}
